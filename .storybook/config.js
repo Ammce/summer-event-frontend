3efa5ee5038cd7df1eb5b575b55cf76530472182
rukfas <farukbruncevic@rocketmail.com>
2020-03-10 02:38:36 +0100

    delete this file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,16 +1,15 @@
-import requireContext from 'require-context.macro';
-import { configure, addDecorator } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import './index.css'
+// import requireContext from 'require-context.macro';
+// import { configure, addDecorator } from '@storybook/react';
+// import './index.css'
 
-// automatically import all files ending in *.stories.js
-const req = requireContext('../src/stories', true, /\.stories\.(js|mdx)$/);
-function loadStories() {
-  req
-    .keys()
-    .sort()
-    .forEach(filename => req(filename));
-}
+// // automatically import all files ending in *.stories.js
+// const req = requireContext('../src/stories', true, /\.stories\.(js|mdx)$/);
+// function loadStories() {
+//   req
+//     .keys()
+//     .sort()
+//     .forEach(filename => req(filename));
+// }
 
-addDecorator(withInfo);
-configure(loadStories, module);
+// // addDecorator(withInfo);
+// configure(loadStories, module);
